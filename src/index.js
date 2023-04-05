3d7605dfef13834e1f339ef1b9fdb9342d327cc5
juliovianadev <juliovianadev@gmail.com>
2023-04-05 15:52:06 -0300

    bugou aqui

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import ProductsProvider from './context/products-context'
 import './index.css';
 import App from './App';
+import configureStore from './hooks-store/products-store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+ 
+configureStore()
 root.render(
   <ProductsProvider>
     <BrowserRouter>
