dc2da15ae6f14c10db79707fd5cd957c75c97feb
juliovianadev <juliovianadev@gmail.com>
2023-04-04 23:14:19 -0300

    updating to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -11,14 +11,13 @@ import productReducer from './store/reducers/products';
 const rootReducer = combineReducers({
   shop: productReducer
 });
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = createStore(rootReducer);
-
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
 );
+
